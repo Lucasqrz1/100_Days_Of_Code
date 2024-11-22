e219e7b30efc615361f45ba0b4f65031b61cf27f
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-22 16:13:44 -0300

    Update main.py

--- Higher_or_Lower_Project/main.py
+++ Higher_or_Lower_Project/main.py
@@ -2,13 +2,41 @@ from art import logo, vs
 from game_data import data
 import random
 
+
+def get_random_data():
+    return random.choice(data)
+
+def format_data(item):
+    return f"{item['name']}, a {item['description']}, from {item['country']}"
+
 def game():
-    for item in data:
-        print(logo)
-        compare_a = data[0]
-        compare_b = data[1]
-        print(f"Compare A: {compare_a['name']},a {compare_a['follower_count']}, from{compare_a['description', 'country']}")
-        print (vs)
-        print(f"Compare B: {compare_b['name']},a {compare_b['follower_count']}, from{compare_b['description', 'country']}")
+    print(logo)
+    score = 0
+    game_continues = True
+
+    compare_a = get_random_data()
+    compare_b = get_random_data()
+
+    while compare_a == compare_b:
+        compare_b =get_random_data()
+
+    while game_continues:
+        compare_a = compare_b
+        compare_b = get_random_data()
+
+        compare_a = get_random_data()
+        compare_b = get_random_data()
+        print(f"Compare A: {format_data(compare_a)}")
+        print(vs)
+        print(f"Compare B: {format_data(compare_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+        a_follower_count = compare_a['follower_count']
+        b_follower_count = compare_b['follower_count']
+
+        is_correct = (guess == 'A' and a_follower_count > b_follower_count)
+
+
 
 game()
