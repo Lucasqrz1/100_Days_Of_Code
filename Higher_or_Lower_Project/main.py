c3ab4c2d3b24e91da1867fca8bf2bf846516f128
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-23 14:59:01 -0300

    Update main.py
    Final changes

--- Higher_or_Lower_Project/main.py
+++ Higher_or_Lower_Project/main.py
@@ -1,14 +1,12 @@
 from art import logo, vs
 from game_data import data
 import random
-
-
+#Get a random data from game_data file
 def get_random_data():
     return random.choice(data)
-
+#Format the data retrieved to print
 def format_data(item):
     return f"{item['name']}, a {item['description']}, from {item['country']}"
-
 def game():
     print(logo)
     score = 0
@@ -24,8 +22,6 @@ def game():
         compare_a = compare_b
         compare_b = get_random_data()
 
-        compare_a = get_random_data()
-        compare_b = get_random_data()
         print(f"Compare A: {format_data(compare_a)}")
         print(vs)
         print(f"Compare B: {format_data(compare_b)}")
@@ -35,8 +31,12 @@ def game():
         a_follower_count = compare_a['follower_count']
         b_follower_count = compare_b['follower_count']
 
-        is_correct = (guess == 'A' and a_follower_count > b_follower_count)
-
-
-
+        is_correct = (guess == 'A' and a_follower_count > b_follower_count) or (guess == 'B' and a_follower_count < b_follower_count)
+        if is_correct:
+            score += 1
+            print("\n" * 20)
+            print(f"You're right! Current score: {score}")
+        else:
+            game_continues = False
+            print(f"Sorry. That's wrong. Final score: {score}")
 game()
