b86c58d338bb535d06aeae684dea19fc1f8bf64d
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-24 18:57:50 -0300

    Update main.py

--- Coffee_Machine_Project/main.py
+++ Coffee_Machine_Project/main.py
@@ -29,3 +29,27 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+water_available = 100
+milk_available = 200
+coffee_available = 100
+money = 0
+
+#Print report
+def report():
+    print(f"Water: {water_available}ml")
+    print(f"Milk: {milk_available}ml")
+    print(f"Coffee: {coffee_available}g")
+    print(f"Money: ${money}")
+report()
+
+#Ask customer for order
+order = input("What would you like? (espresso/latte/cappuccino):")
+
+#Check resources sufficient?
+
+#Process coins
+
+#Check transaction successful?
+
+#Make the coffee/deduct the resources
+
