e5de7575a50b945ce321d58b09f3ae35865d8f5d
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-30 16:44:02 -0300

    Update question_model.py

--- Day-17/Quiz_Game/question_model.py
+++ Day-17/Quiz_Game/question_model.py
@@ -3,9 +3,4 @@ class Question:
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
-#Generating the question from the database
-def get_question(User):
-    get_question.text
-    get_question.answer
-    input("Question")
 
