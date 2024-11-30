419ac47f8de2aa8d75d84f21b6a302dd291a95bc
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-30 17:24:41 -0300

    Update main.py

--- Day-17/Quiz_Game/main.py
+++ Day-17/Quiz_Game/main.py
@@ -9,3 +9,9 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+#Asking the questions
+#Checking if the answer was correct
+#Checking if we're the end of the quiz
+
+
