5793e2a809f77eb4d1af572ea9fff01a49e165fb
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-11-30 17:25:13 -0300

    Create quiz_brain.py

---
+++ Day-17/Quiz_Game/quiz_brain.py
@@ -0,0 +1,11 @@
+#Create Class
+class QuizBrain:
+    def __init__(self):
+        self.question_number = 0
+        self.question_list = q_list
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+
+
+
