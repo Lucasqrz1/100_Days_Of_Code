04802756e1e79aa83d52ca24f61d5ba6c8d6e4be
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-03 18:48:26 -0300

    Final changes

--- Day-17/Quiz_Game/main.py
+++ Day-17/Quiz_Game/main.py
@@ -12,10 +12,10 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
 
-#Asking the questions
-#Checking if the answer was correct
-#Checking if we're the end of the quiz
+#if quizz still has questions remaining:
+while quiz.still_has_questions():
+    quiz.next_question()
 
-
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score} / {len(question_bank)}")
