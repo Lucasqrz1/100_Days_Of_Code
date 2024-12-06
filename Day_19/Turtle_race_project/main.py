388db0ffa83e4b87a0c75e9d58de97552203103c
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-06 17:45:46 -0300

    Tests

--- Day_19/Turtle_race_project/main.py
+++ Day_19/Turtle_race_project/main.py
@@ -8,7 +8,31 @@ tim.color("purple")
 def move_forwards():
     tim.forward(10)
 
+#Create the function with the given 'onkey' arguments:
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+def jump():
+    tim.penup()
+    tim.forward(20)
+    tim.pendown()
+
+num = []
+for i in range (10):
+    num.append(tim)
+
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(move_backwards, "s")
+screen.onkey(jump, "space")
 screen.exitonclick()
