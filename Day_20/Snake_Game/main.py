fbaeb70f6dd866052062042c75261ee49b7970d9
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-13 18:26:52 -0300

    Update main.py

--- Day_20/Snake_Game/main.py
+++ Day_20/Snake_Game/main.py
@@ -1,7 +1,10 @@
 from turtle import Screen, Turtle
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
+
+from sqlalchemy.util import ScopedRegistry
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -28,7 +32,24 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        print("nom nom nom")
+        snake.extend()
+        scoreboard.increase_score()
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    #Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    #if head collides with any segment in the tail
+        #trigger game_over
+
+
+
 
 
 screen.exitonclick()
