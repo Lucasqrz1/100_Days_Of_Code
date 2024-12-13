9c040ac2736098e37d6ffa008327ce4d2b24a651
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-13 16:40:50 -0300

    Update main.py

--- Day_20/Snake_Game/main.py
+++ Day_20/Snake_Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +24,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        print("nom nom nom")
+
 
 screen.exitonclick()
