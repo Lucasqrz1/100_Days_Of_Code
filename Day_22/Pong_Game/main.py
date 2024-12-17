5455038e0efb23f0243f65f5444babb1db1cdc8f
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-17 15:36:10 -0300

    Update main.py

--- Day_22/Pong_Game/main.py
+++ Day_22/Pong_Game/main.py
@@ -2,6 +2,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+
 import time
 
 screen = Screen()
@@ -13,7 +15,7 @@ screen.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(l_paddle.go_up,"w")
@@ -39,5 +41,16 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() <-320:
         print("Made contact")
         ball.bounce_x()
+        time.sleep(ball.move_speed)
+
+    #Detect R paddle misses:
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    #Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
