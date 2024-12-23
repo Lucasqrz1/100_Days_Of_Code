024cf4da57789a373d4fef01e254df330ca855a6
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-23 17:29:29 -0300

    Update player.py

--- Day__23/player.py
+++ Day__23/player.py
@@ -1,7 +1,13 @@
+from turtle import Turtle, Screen
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+
+    def is_at_finish_line(self):
+        self.
