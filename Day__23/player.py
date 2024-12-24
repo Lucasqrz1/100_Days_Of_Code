497f5a6a8ee4d364b8984ee827ad7a505903afc3
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-24 02:11:02 -0300

    Update player.py

--- Day__23/player.py
+++ Day__23/player.py
@@ -1,13 +1,26 @@
 from turtle import Turtle, Screen
 
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 20
 FINISH_LINE_Y = 280
+
+screen = Screen()
 
 
 class Player(Turtle):
     def __init__(self):
         super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
 
-    def is_at_finish_line(self):
-        self.
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_at_finish(self):
+        return self.ycor() > FINISH_LINE_Y
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
