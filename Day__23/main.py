fd32c5cd70b0bfb2e33032d642e7f4d274bd1ade
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-24 02:10:41 -0300

    Update main.py

--- Day__23/main.py
+++ Day__23/main.py
@@ -1,49 +1,41 @@
 import time
-from player import Screen, Turtle
+from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 level = 0
-
+#screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-screen.title(f"Level: {level}")
+screen.title("Turtle Crossing Game")
 
-car = CarManager()
-#player = Player()
-#scoreboard = Scoreboard()
-
-player = Turtle()
-player.color("Black")
-player.shape("turtle")
-player.setheading(90)
-player.penup()
-player.goto(0, -270)
-
-
-def move_up(): player.forward(15)
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(move_up, "Up")
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car.create_car()
-    car.move_cars()
+    car_manager.create_car()
+    car_manager.move_cars()
+
     # Detect collision with car
-    for car in car.all_cars:
+    for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
-            #scoreboard.game_over()
+            scoreboard.game_over()
+
     # Detect if player has reached the other side
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car.increase_speed()
-        #scoreboard.increase_level()
+    if player.is_at_finish():
+       player.go_to_start()
+       car_manager.increase_speed()
+       scoreboard.increase_level()
 
 screen.exitonclick()
 
