7dffae244853e28ba718da512ce87107beaa9ff6
Lucas Queiroz <lucasqrz1@hotmail.com>
2024-12-24 02:10:51 -0300

    Update car_manager.py

--- Day__23/car_manager.py
+++ Day__23/car_manager.py
@@ -1,38 +1,43 @@
-import random
-from turtle import Turtle
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+level = 0
+#screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.title("Turtle Crossing Game")
 
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
 
-class CarManager(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.all_cars = []
-        self.car_speed = 5
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
 
-    def create_car(self):
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
-        chance = random.randint(1, 6)
-        if chance == 1:  # Adjust the frequency of car generation
-            new_car = Turtle("square")
-            new_car.shapesize(stretch_wid=1, stretch_len=2)
-            new_car.penup()
-            new_car.color(random.choice(COLORS))
-            random_y = random.randint(-250,250)
-            new_car.goto(300, random_y)
-            self.all_cars.append(new_car)
+    # Detect if player has reached the other side
+    if player.is_at_finish():
+       player.go_to_start()
+       car_manager.increase_speed()
+       scoreboard.increase_level()
 
-
-    def move_cars(self):
-        for car in self.all_cars:
-            car.backward(self.car_speed)
-
-    def increase_speed(self):
-        self.car_speed += 2
-
+screen.exitonclick()
 
 
 
