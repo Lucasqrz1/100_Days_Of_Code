a90c32035087c87a13d9a574c034ca41dc75c302
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-01-31 17:22:25 -0300

    Update main.py

--- Day_25/main.py
+++ Day_25/main.py
@@ -17,8 +17,46 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-#Experimenting Pandas
 import pandas
-
+#
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# # print(data["temp"])
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+#
+# # Calculating average
+# average = sum(temp_list) / len (temp_list)
+# print(average)
+#
+# #Another form of calculating average
+# print(data["temp"].mean())
+#
+# # Get max value
+# print(data["temp"].max())
+#
+# #Get data in columns
+# print(data["condition"])
+# # or
+# print(data.condition)
+
+#Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# converting to Fahrenheit
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0]
+monday_temp_F = monday_temp * 9/5 +32
+print(monday_temp_F)
+
+# Creat dataframe from scratch
+data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76,56,65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+print(data)
