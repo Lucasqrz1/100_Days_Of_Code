765b2990becc79d5878820c18aa95a01fe33e04c
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-01-31 16:43:24 -0300

    Update main.py

--- Day_25/main.py
+++ Day_25/main.py
@@ -5,14 +5,20 @@
 #     data = data_file.readlines()
 #     print(data)
 
+#
+# # Selecting specific data
+# import csv
+#
+# with open ("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-# Selecting specific data
-import csv
+#Experimenting Pandas
+import pandas
 
-with open ("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
