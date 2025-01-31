95defb8f6c59841f2e3864743166ed74c728ce85
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-01-31 16:22:01 -0300

    Update main.py

--- Day_25/main.py
+++ Day_25/main.py
@@ -1,4 +1,18 @@
 
-with open ("weather_data - Sheet1.csv") as file:
-    data = file.read()
-    print(data)
+## Opening and printing data as a list
+
+# with open ("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+
+# Selecting specific data
+import csv
+
+with open ("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
