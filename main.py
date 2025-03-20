4fc01364d125ae9cab1f2fb57aabb1be506b919e
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-03-20 14:09:59 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state =
+
+turtle.mainloop()
