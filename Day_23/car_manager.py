a943883c462d8aa64c3dd75828e42e60e9408c3b
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-03-21 16:13:26 -0300

    Delete Day_23 directory

--- Day_23/car_manager.py
+++
@@ -1,9 +0,0 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
-
-
-class CarManager:
-    pass
-
-#start back from here and merge files
