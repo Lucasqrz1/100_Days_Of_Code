580201b4bec3644bcd4d2d76f13cd7e28f77cc63
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-04-19 13:15:11 -0300

    New Features

--- Day_27/.venv/main.py
+++ Day_27/.venv/main.py
@@ -1,18 +1,36 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # Label
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label["text"] = "New_Text"
+my_label.config(text="New Text")
+
+# Button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+    return
+
+button = Button(text="Click me",command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 
-
-
+#
 
 
 
