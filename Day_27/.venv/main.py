b5c101b0baac4f61b33f158de6fcd75f5a340a2f
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-04-19 13:56:06 -0300

    New Features

--- Day_27/.venv/main.py
+++ Day_27/.venv/main.py
@@ -5,12 +5,11 @@ window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # Label
-
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "New_Text"
 my_label.config(text="New Text")
+# my_label.pack(side="left")
+# my_label.place(x=100,y=100)
+my_label.grid(column=0, row=0)
 
 # Button
 
@@ -21,12 +20,19 @@ def button_clicked():
     return
 
 button = Button(text="Click me",command=button_clicked)
-button.pack()
+# button.pack()
+button.grid(column=1, row=1)
 
 # Entry
 
 input = Entry(width=10)
-input.pack()
+# input.pack()
 print(input.get())
+input.grid(column=2, row=2)
+
+# Challenge
+button_2 = Button(text="New Button",command=button_clicked)
+
+button_2.grid(column=3, row=0)
 
 window.mainloop()
