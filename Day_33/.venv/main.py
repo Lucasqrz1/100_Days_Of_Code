23515c3e75cfe1b3c3db18e2e4c37fcd4d1e46a1
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-04-28 23:33:17 -0300

    New project

--- Day_33/.venv/main.py
+++ Day_33/.venv/main.py
@@ -1,5 +1,8 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-if response.status_code != 200:
-    raise Exception("Bad response from ISS API")
+response.raise_for_status()
+
+data = response.json()["iss_position"]
+print(data)
+
