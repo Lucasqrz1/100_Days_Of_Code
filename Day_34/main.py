ccf527d6710cc98aa7eb2abb603aa9e79c36a7b6
Lucas Queiroz <lucasqrz1@hotmail.com>
2025-05-02 18:00:56 -0400

    Update main.py

--- Day_34/main.py
+++ Day_34/main.py
@@ -17,5 +17,5 @@ quiz_ui = QuizInterface(quiz)
 while quiz.still_has_questions():
     quiz.next_question()
 
-print("You've completed the quiz")
+print("You've completed the quiz!")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
