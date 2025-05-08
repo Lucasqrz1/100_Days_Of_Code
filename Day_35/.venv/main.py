8cafc19be56832146f83657a33074ab35145797f
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-05-08 15:42:12 -0300

    New project

---
+++ Day_35/.venv/main.py
@@ -0,0 +1,30 @@
+import requests
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "insert api key here"
+
+weather_params = {
+    "lat": 42.679909,
+    "lon": -70.837341,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+
+response = requests.get(OWM_Endpoint, params=weather_params )
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = print(hour_data ["weather"][0]["id"])
+    if int(condition_code) < 700:
+        will_rain = True
+
+    if will_rain:
+        print("Bring an umbrella.")
+
+
+
