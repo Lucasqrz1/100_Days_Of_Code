83bad62d976f0a41c15310a564cb867d709a2ce2
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-05-13 00:57:52 -0300

    New project

---
+++ Day_39/.venv/flight_search.py
@@ -0,0 +1,6 @@
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
