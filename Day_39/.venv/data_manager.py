d2f9b866771d3381b19d1ab040afcdcde8ae35c1
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-05-13 11:17:34 -0300

    Final Changes

--- Day_39/.venv/data_manager.py
+++ Day_39/.venv/data_manager.py
@@ -8,25 +8,24 @@ load_dotenv()
 
 SHEETY_PRICES_ENDPOINT = YOUR ENDPOINT HERE
 
-
 class DataManager:
 
     def __init__(self):
-        self._user = os.environ["SHEETY_USRERNAME"]
+        self._user = os.environ["SHEETY_USERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
         self.destination_data = {}
 
     def get_destination_data(self):
-        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
         # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
         # pprint(data)
         return self.destination_data
 
-    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
     # to update the Google Sheet with the IATA codes. (Do this using code).
     def update_destination_codes(self):
         for city in self.destination_data:
@@ -37,7 +36,6 @@ class DataManager:
             }
             response = requests.put(
                 url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
-                json=new_data,
-                auth=self._authorization
+                json=new_data
             )
             print(response.text)
--- Day_39/.venv/main.py
+++ Day_39/.venv/main.py
@@ -1,21 +1,45 @@
-# 4. Pass the data back to the main.py file, so that you can print the data from main.py
+import time
+from datetime import datetime, timedelta
 from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+
+# ==================== Set up the Flight Search ====================
+
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
+flight_search = FlightSearch()
 
-#  5. In main.py check if sheet_data contains any values for the "iataCode" key.
-#  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, pass each city name in sheet_data one-by-one
-#  to the FlightSearch class to get the corresponding IATA code
-#  for that city using the Flight Search API.
-#  You should use the code you get back to update the sheet_data dictionary.
-if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-    for row in sheet_data:
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
+for row in sheet_data:
+    if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+# ==================== Search for Flights ====================
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
