8cc689cb39902818d365f393594c580ec5bb4a45
Lucasqrz1 <lucasqrz1@hotmail.com>
2025-06-11 15:27:26 -0300

    "final changes"

--- Day_66/main.py
+++ Day_66/main.py
@@ -5,12 +5,15 @@ from sqlalchemy import Integer, String, Boolean
 import random
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TopSecretAPIKey'
+
 
 # CREATE DB
 class Base(DeclarativeBase):
     pass
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -33,11 +36,10 @@ class Cafe(db.Model):
         # Using dictionary comprehension
         return{column.name:getattr(self, column.name) for column in self.__table__.columns}
 
-
 with app.app_context():
     db.create_all()
 
-
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -45,6 +47,8 @@ def home():
 
 
 # HTTP GET - Read Record
+
+# Random Cafe
 
 @app.route("/random", methods=["GET"])
 # Not necessary  to input the GET method all the time, it comes with the decorator
@@ -55,6 +59,7 @@ def get_random_cafe():
     # Converting the data to a dictionary
     return jsonify(cafe=random_cafe.to_dict())
 
+# All Cafes
 @app.route("/all")
 def all_cafes():
     result = db.session.execute(db.select(Cafe))
@@ -62,6 +67,7 @@ def all_cafes():
     # returning a list of dictionaries through dictionary comprehension
     return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
+# Search a cafe by location
 @app.route("/search")
 def get_cafe_at_location():
     query_location = request.args.get("loc")
@@ -75,6 +81,7 @@ def get_cafe_at_location():
 
 # HTTP POST - Create Record
 
+# Add a new cafe
 @app.route("/add", methods=["POST"])
 def post_new_cafe():
     new_cafe = Cafe(
@@ -89,8 +96,6 @@ def post_new_cafe():
         seats=request.form.get("seats"),
         coffee_price=request.form.get("coffee_price"),
     )
-    if not request.form.get("name") or not request.form.get("map_url"):
-        return jsonify(error={"error": "Missing required fields."}), 400
     db.session.add(new_cafe)
     db.session.commit()
     return jsonify(response={"success": "Successfully added the new cafe."})
@@ -98,7 +103,35 @@ def post_new_cafe():
 
 # HTTP PUT/PATCH - Update Record
 
+# Update a cafe price by id
+@app.route("/patch_new_price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.execute(db.select(Cafe).where(Cafe.id == cafe_id)).scalar()
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": f"Successfully updated the price: {new_price}."}), 200
+
+    else:
+
+        return jsonify(error={"Not Found": "Sorry a cafe with that id is not found!"}), 404
+
 # HTTP DELETE - Delete Record
+
+# Delete a cafe by id
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key != "TopSecretAPIKey":
+        return jsonify(error={"error": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+    cafe = db.session.get(Cafe, cafe_id)
+    if cafe:
+        db.session.delete(cafe)
+        db.session.commit()
+        return jsonify(response={"success": "Successfully deleted the cafe."})
+    else:
+        return jsonify(error={"error": "Sorry a cafe with that id was not found in the database."}), 404
 
 
 if __name__ == '__main__':
